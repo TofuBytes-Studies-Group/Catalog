a6ffe1fdf2a58f60ab3651cb9e551f447c670874
unknown <carolinehoeg123@gmail.com>
2024-12-04 13:38:06 +0100

    Changed Restaurant to ReataurantId

--- Catalog.DTO/DTO/CatalogResponse.cs
+++ Catalog.DTO/DTO/CatalogResponse.cs
@@ -5,12 +5,12 @@ public class CatalogResponse
     public Guid CustomerId { get; set; }
     
     public string CustomerUsername { get; set; }
-    public Guid Restaurant { get; set; }
+    public Guid RestaurantId { get; set; }
     public List<DishResponse> Dishes { get; set; }
     
     public CatalogResponse(Guid restaurantId, Guid customerId, string customerUsername, List<DishResponse> dishes)
     {
-        Restaurant = restaurantId;
+        RestaurantId = restaurantId;
         CustomerId = customerId;
         CustomerUsername = customerUsername;
         Dishes = dishes;
